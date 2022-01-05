9968615562ee5d5f3c30bbf92b2a6a7270d71409
Aleksandra Azarova <alekssaffl@yandex.ru>
2022-01-05 06:45:14 +0300

    feat: run webpack from server

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,45 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
+import webpack from 'webpack';
+
+const config = require('../webpack.prod');
 
 const app = express();
 const port = 4000;
+
+const compiler = webpack(config);
+
+compiler.run((err, stats) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  if (!stats) return;
+  const info = stats.toJson();
+
+  if (stats.hasErrors()) {
+    console.error(info.errors);
+  }
+
+  if (stats.hasWarnings()) {
+    console.warn(info.warnings);
+  }
+
+  console.log(
+    stats.toString({
+      colors: true,
+    }),
+  );
+
+  compiler.close((closeErr) => {
+    if (closeErr) {
+      console.error(closeErr);
+    }
+    console.log('Compilation ended');
+  });
+});
 
 app.get(/\.(js|css|map|ico)$/, express.static(path.resolve(__dirname, '../public')));
 app.use('*', (request, response) => {
