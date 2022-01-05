5abc10f6651ae9e6c9ec58da7c419c5f5676e23d
Aleksandra Azarova <alekssaffl@yandex.ru>
2022-01-05 05:34:43 +0300

    feat: serving static files

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+app.get(/\.(js|css|map|ico)$/, express.static(path.resolve(__dirname, '../public')));
+app.use('*', (request, response) => {
+  const indexHTML = fs.readFileSync(path.resolve(__dirname, '../public/index.html'));
+
+  response.contentType('text/html').status(200).send(indexHTML);
 });
 
 app.listen(port, () => {
